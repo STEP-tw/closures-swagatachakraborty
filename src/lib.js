d76f6bea0c82c624a3362eb9d7223edcec83619f
Swagata Chakraborty <cswagata@IN-swagata.local>
2018-10-25 11:49:53 +0530

    makeDeltaTracker() is define in lib.js

--- src/lib.js
+++ src/lib.js
@@ -19,7 +19,21 @@ const makeCounterFromZero =  function(){
   }
 }
 
-const makeDeltaTracker = undefined;
+const makeDeltaTracker = function(initValue) {
+  let deltaTrack = { old : initValue,
+    delta : 0, 
+    new : initValue };
+  return function(deltaValue ) {
+    if( deltaValue == undefined ) {
+      return deltaTrack;
+    }
+    deltaTrack["old"] = deltaTrack["new"];
+    deltaTrack["delta"] = deltaValue;
+    deltaTrack["new"] = deltaTrack["new"] + deltaTrack["delta"];
+    return deltaTrack;
+  }
+}
+
 const makeFiboGenerator = undefined;
 const makeCycler = undefined;
 const curry = undefined;
