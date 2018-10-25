030ac6ded28e4f64052d24a540a2c123fca4be43
Swagata Chakraborty <cswagata@IN-swagata.local>
2018-10-25 14:06:48 +0530

    some variable name changed.

--- src/lib.js
+++ src/lib.js
@@ -1,12 +1,14 @@
 const makeConstant = function (givenValue){
-  const source = givenValue;
+  const fixedValue = givenValue;
+
   return function(){
-    return source;
+    return fixedValue;
   }
 }
 
 const makeCounterFromN = function(initValue){
   let count = initValue;
+
   return function(){
     return count++;
   }
@@ -14,6 +16,7 @@ const makeCounterFromN = function(initValue){
 
 const makeCounterFromZero =  function(){
   let count = 0;
+
   return function(){
     return count++;
   }
@@ -23,6 +26,7 @@ const makeDeltaTracker = function(initValue) {
   let deltaTrack = { old : initValue,
     delta : 0, 
     new : initValue };
+
   return function(deltaValue ) {
     if( deltaValue == undefined ) {
       return deltaTrack;
@@ -52,6 +56,7 @@ const makeFiboGenerator = function(current = 1, old = 0) {
 
 const makeCycler = function(items) {
   let cycle = items.toString().split(",");
+
   return function () {
     let currentValue = cycle.shift();
     cycle.push(currentValue);
@@ -61,6 +66,7 @@ const makeCycler = function(items) {
 
 const curry = function(func, initValue) {
   let arg1 = initValue;
+  
   return function (arg2, arg3) {
     let result = func(arg1,arg2,arg3);
     return result;
