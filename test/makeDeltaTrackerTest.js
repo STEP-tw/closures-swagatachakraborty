b00746bda16256494dfa8beef36643d74f567792
Swagata Chakraborty <cswagata@IN-swagata.local>
2018-10-25 15:26:53 +0530

    new test case is added

--- test/makeDeltaTrackerTest.js
+++ test/makeDeltaTrackerTest.js
@@ -17,12 +17,29 @@ const testTrackerWithNegativeDeltaValue = function() {
   assert.deepEqual({old: 1, delta: -1, new: 0}, trackDelta(-1));
   assert.deepEqual({old: 0, delta: -2, new: -2}, trackDelta(-2));
 };
+const testTracker = function() {
+ let trackDelta = makeDeltaTracker(3);
+ let result = trackDelta(1);
+ assert.deepEqual({old: 3, delta: 1, new: 4}, result);
+ trackDelta(3)
+ assert.deepEqual({old: 3, delta: 1, new: 4}, result);
+}
+
+const testTrackerWithDeltaValueThatIsHoldByAVariable = function() {
+  let init = 1;
+  let trackDelta = makeDeltaTracker(init);
+  let result =  assert.deepEqual({old: 1, delta: -1, new: 0}, trackDelta(-1));
+  result["old"] = 2;
+  assert.deepEqual({old: 0, delta: -2, new: -2}, trackDelta(-2));
+};
 
 const runTests = function() {
   if(!makeDeltaTracker) {
     console.log("Function 'makeDeltaTracker' in not implemented yet. Not running tests.");
     return;
   }
+  testTracker();
+  console.log("testTracker() passed");
 
   testTrackerWithDefaultDeltaZero();
   console.log("testTrackerWithDefaultDeltaZero passed");
